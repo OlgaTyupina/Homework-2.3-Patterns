7f74bcafd8429730da266bac1326fcd415a0100f
OlgaTyupina <57899824+OlgaTyupina@users.noreply.github.com>
2019-12-26 14:18:00 +0300

    Update TestCardDelivery.java

--- src/test/java/TestCardDelivery.java
+++ src/test/java/TestCardDelivery.java
@@ -13,16 +13,15 @@ public class TestCardDelivery {
     @Test
     void shouldFillInTheForm() {
         open("http://localhost:9999");
-        SelenideElement form = $ ("form.form");
-        form.$("[data-test-id=city] input").setValue(DataGeneration.getCity());
-        form.$("[data-test-id=date] input").setValue(DataGeneration.getNewDate());
-        form.$("[data-test-id=name] input").setValue(DataGeneration.getName());
-        form.$("[data-test-id=phone] input").setValue(DataGeneration.getPhone());
-        form.$("[data-test-id=agreement]").click();
+        $("[data-test-id=city] input").setValue(DataGeneration.getCity());
+        $("[data-test-id=date] input").setValue(DataGeneration.getNewDate());
+        $("[data-test-id=name] input").setValue(DataGeneration.getName());
+        $("[data-test-id=phone] input").setValue(DataGeneration.getPhone());
+        $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Запланировать")).click();
         $("[data-test-id=success-notification]").waitUntil(visible, 15000);
         DataGeneration.cleanUp();
-        form.$("[data-test-id=date] input").setValue(DataGeneration.getFutureDate());
+        $("[data-test-id=date] input").setValue(DataGeneration.getFutureDate());
         $(".button__text").click();
         $("[data-test-id='replan-notification']").waitUntil(Condition.visible,
                 5000);
