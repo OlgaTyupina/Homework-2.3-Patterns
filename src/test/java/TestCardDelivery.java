4beeac05542ff27098729abd6ef5c105d9bcb400
OlgaTyupina <57899824+OlgaTyupina@users.noreply.github.com>
2019-12-26 14:35:28 +0300

    Update TestCardDelivery.java

--- src/test/java/TestCardDelivery.java
+++ src/test/java/TestCardDelivery.java
@@ -4,7 +4,6 @@ import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -12,15 +11,15 @@ public class TestCardDelivery {
     @Test
     void shouldFillInTheForm() {
         open("http://localhost:9999");
-        $("[placeholder='Город']").setValue(DataGeneration.getCity());
-        $("[placeholder='Дата встречи']").setValue(DataGeneration.getNewDate());
+        $("[data-test-id=city] input").setValue(DataGeneration.getCity());
+        $("[data-test-id=date] input").setValue(DataGeneration.getNewDate());
         $("[data-test-id=name] input").setValue(DataGeneration.getName());
         $("[data-test-id=phone] input").setValue(DataGeneration.getPhone());
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Запланировать")).click();
         $("[data-test-id=success-notification]").waitUntil(visible, 15000);
         DataGeneration.cleanUp();
-        $("[placeholder='Дата встречи']").setValue(DataGeneration.getFutureDate());
+        $("[data-test-id=date] input").setValue(DataGeneration.getFutureDate());
         $(".button__text").click();
         $("[data-test-id='replan-notification']").waitUntil(Condition.visible,
                 5000);
