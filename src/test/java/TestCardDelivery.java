ab50282829e0398bf0a63e5abdc67fae80cc5782
OlgaTyupina <57899824+OlgaTyupina@users.noreply.github.com>
2019-12-30 12:39:41 +0300

    Update TestCardDelivery.java

--- src/test/java/TestCardDelivery.java
+++ src/test/java/TestCardDelivery.java
@@ -11,15 +11,16 @@ public class TestCardDelivery {
     @Test
     void shouldFillInTheForm() {
         open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue(DataGeneration.getCity());
-        $("[data-test-id=date] input").setValue(DataGeneration.getNewDate());
+        $("[placeholder ='Город']").setValue(DataGeneration.getCity());
+        DataGeneration.cleanUp();
+        $("[placeholder='Дата встречи']").setValue(DataGeneration.getNewDate());
         $("[data-test-id=name] input").setValue(DataGeneration.getName());
         $("[data-test-id=phone] input").setValue(DataGeneration.getPhone());
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Запланировать")).click();
         $("[data-test-id=success-notification]").waitUntil(visible, 15000);
         DataGeneration.cleanUp();
-        $("[data-test-id=date] input").setValue(DataGeneration.getFutureDate());
+        $("[placeholder='Дата встречи']").setValue(DataGeneration.getFutureDate());
         $(".button__text").click();
         $("[data-test-id='replan-notification']").waitUntil(Condition.visible,
                 5000);
